34dfbd08251ce82ac93b858c43842d31a216c3b7
DevBack <heltonlima.ti@live.com>
2023-01-12 13:25:48 -0300

    Initial Commit

---
+++ src/main/java/com/devback/domain/DomainPedidosApplication.java
@@ -0,0 +1,13 @@
+package com.devback.domain;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DomainPedidosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DomainPedidosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/devback/domain/DomainPedidosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devback.domain;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DomainPedidosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
